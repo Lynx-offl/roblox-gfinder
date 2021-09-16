0d8ddb7d1019938034c8581a6ec589f3cddd4bd1
Karthi <86093775+Lynx-offl@users.noreply.github.com>
2021-09-16 10:39:50 +0530

    upload files

---
+++ README.md
@@ -0,0 +1,29 @@
+# roblox-group-scanner-v2
+Python 3 tool for finding unclaimed groups on Roblox. Supports multi-threading, multi-processing and HTTP proxies.
+
+Unlike [roblox-group-scanner](https://github.com/h0nde/roblox-group-scanner), this tool uses the batch endpoint for group details, thus resulting in performance speed-ups of up to 100x.
+
+# Usage
+```bash
+python scanner.py -w 16 -r 1-11500000 -c 11000000 -p proxies.txt -u WEBHOOKURL
+```
+
+```
+  -t THREADS, --threads THREADS
+                        amount of threads per worker
+  -w WORKERS, --workers WORKERS
+                        amount of workers (processes)
+  -r RANGE, --range RANGE
+                        range of group ids to be scanned
+  -c CUT_OFF, --cut-off CUT_OFF
+                        group ids past this point won't be blacklisted based on their current validity status
+  -p PROXY_FILE, --proxy-file PROXY_FILE
+                        list of HTTP proxies, separated by newline
+  -u WEBHOOK_URL, --webhook-url WEBHOOK_URL
+                        found groups will be posted to this url
+  --chunk-size CHUNK_SIZE
+                        amount of groups to be sent per API request
+  --get-funds GET_FUNDS
+                        attempt to obtain amount of funds in a group
+  --timeout TIMEOUT     timeout for server connections and responses
+```
---
+++ lib/threads.py
@@ -0,0 +1,171 @@
+from .utils import create_ssl_socket, shutdown_socket, make_embed, send_webhook
+from zlib import decompress
+from itertools import cycle
+try:
+    from orjson import loads as json_loads
+except ImportError:
+    from json import loads as json_loads
+
+GROUP_IGNORED = 0
+GROUP_TRACKED = 1
+
+def thread_func(thread_num, worker_num, thread_barrier, thread_event,
+                check_counter, ssl_context, proxy_iter,
+                gid_range, gid_cutoff, gid_chunk_size,
+                get_funds, webhook_url, timeout):
+    gid_iter = cycle(range(*gid_range))
+    gid_count = gid_range[1] - gid_range[0]
+    gid_cache = {}
+    gid_chunk = None
+
+    thread_barrier.wait()
+    thread_event.wait()
+
+    while True:
+        try:
+            proxy_addr = next(proxy_iter)
+        except StopIteration:
+            proxy_addr = None
+
+        try:
+            sock = create_ssl_socket(
+                ("groups.roblox.com", 443),
+                ssl_context=ssl_context,
+                proxy_addr=proxy_addr,
+                timeout=timeout)
+        except:
+            continue
+        
+        while True:
+            if not gid_chunk:
+                if gid_chunk_size > gid_count:
+                    # no more un-ignored groups left to scan
+                    # kill thread
+                    return
+                
+                gid_chunk = []
+                while gid_chunk_size > len(gid_chunk):
+                    gid = next(gid_iter)
+                    if gid_cache.get(gid) == GROUP_IGNORED:
+                        continue
+                    gid_chunk.append(gid)
+
+            try:
+                # request bulk group info
+                sock.send(f"GET /v2/groups?groupIds={','.join(map(str, gid_chunk))} HTTP/1.1\n"
+                           "Host:groups.roblox.com\n"
+                           "Accept-Encoding:deflate\n"
+                           "\n".encode())
+                resp = sock.recv(1024 ** 2)
+                if not resp.startswith(b"HTTP/1.1 200 OK"):
+                    raise ConnectionAbortedError(
+                        f"Unexpected response while requesting group details: {resp[:64]}")
+                resp = resp.split(b"\r\n\r\n", 1)[1]
+                # deflate encoded responses always end in null byte
+                while resp[-1] != 0:
+                    resp += sock.recv(1024 ** 2)
+                group_assoc = {
+                    x["id"]: x
+                    for x in json_loads(decompress(resp, -15)[8:-1])
+                }
+
+                for gid in gid_chunk:
+                    group_status = gid_cache.get(gid)
+                    group_info = group_assoc.get(gid)
+
+                    if group_status == GROUP_IGNORED:
+                        continue
+                    
+                    if not group_info:
+                        # info for group wasn't included in response
+                        if not gid_cutoff or gid_cutoff > gid:
+                            # assume it's deleted and ignore it in future requests
+                            gid_cache[gid] = GROUP_IGNORED
+                            gid_count -= 1
+                        continue
+                    
+                    if not group_status:
+                        if group_info["owner"]:
+                            # group has an owner and this is our first time checking it
+                            # add it as a tracked group
+                            gid_cache[gid] = GROUP_TRACKED
+                        else:
+                            # group doesn't have an owner and this is only our first time checking it
+                            # assume it's manual-approval/locked and ignore it in future requests
+                            gid_cache[gid] = GROUP_IGNORED
+                            gid_count -= 1
+                        continue
+
+                    if group_info["owner"]:
+                        # group has an owner and this is *not* our first time checking it
+                        # skip to next group
+                        continue
+                    
+                    # group doesn't have an owner, but it did when we last checked
+                    # request extra info and determine if it's claimable
+                    sock.send(f"GET /v1/groups/{gid} HTTP/1.1\n"
+                               "Host:groups.roblox.com\n"
+                               "\n".encode())
+                    resp = sock.recv(1024**2)
+                    if not resp.startswith(b"HTTP/1.1 200 OK"):
+                        raise ConnectionAbortedError(
+                            f"Unexpected response while requesting extra group details: {resp[:64]}")
+                    group_info = json_loads(resp.split(b"\r\n\r\n", 1)[1])
+
+                    if not group_info["publicEntryAllowed"] \
+                        or group_info["owner"] \
+                        or "isLocked" in group_info:
+                        # group is unclaimable
+                        # ignore group in future requests
+                        gid_cache[gid] = GROUP_IGNORED
+                        gid_count -= 1
+                        continue
+
+                    # get amount of funds in group
+                    if get_funds:
+                        funds_sock = create_ssl_socket(
+                            ("economy.roblox.com", 443),
+                            ssl_context=ssl_context,
+                            proxy_addr=proxy_addr,
+                            timeout=timeout)
+                        try:
+                            funds_sock.send(f"GET /v1/groups/{group_info['id']}/currency HTTP/1.1\n"
+                                            "Host:economy.roblox.com\n"
+                                            "\n".encode())
+                            resp = funds_sock.recv(1024**2)
+                            if resp.startswith(b"HTTP/1.1 200 OK"):
+                                group_info["funds"] = json_loads(resp.split(b"\r\n\r\n", 1)[1])["robux"]
+                            elif not b'"code":3,' in resp:
+                                raise ConnectionAbortedError(
+                                    f"Unexpected response while requesting group fund details: {resp[:64]}")
+                        finally:
+                            shutdown_socket(funds_sock)
+
+                    # log group as claimable
+                    print(" ~ ".join([
+                        f"https://www.roblox.com/groups/{gid}",
+                        f"{group_info['memberCount']} members",
+                        (f"R$ {group_info['funds']}" if group_info.get("funds") is not None else '?') + " funds",
+                        group_info["name"]
+                    ]))
+
+                    if webhook_url:
+                        send_webhook(
+                            webhook_url,
+                            embeds=[make_embed(group_info)]
+                        )
+
+                    # ignore group in future requests
+                    gid_cache[gid] = GROUP_IGNORED
+                    gid_count -= 1
+
+                check_counter.add(len(gid_chunk))
+                gid_chunk = None
+
+            except KeyboardInterrupt:
+                exit()
+            
+            except Exception as err:
+                break
+            
+        shutdown_socket(sock)
